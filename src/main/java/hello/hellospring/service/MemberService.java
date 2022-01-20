bc81531c81c0217bd6ff2f57ad822ce42e35901b
RyuKH95 <gh9503@gmail.com>
2022-01-20 15:33:27 +0900

    TimeTraceAOP AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -26,17 +26,9 @@ public class MemberService {
      * @return getId
      */
     public Long join(Member member) {
-        long start = System.currentTimeMillis();
-
-        try {
-            validateDuplicateMember(member); //중복회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member); //중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -51,15 +43,7 @@ public class MemberService {
      * @return
      */
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
